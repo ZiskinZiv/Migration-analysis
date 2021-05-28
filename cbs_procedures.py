04a3d9c80e4776221311578462cc0d7efba91269
shlomi <shlomiziskin@gmail.com>
2021-05-28 16:15:22 +0300

    added peripheri

--- cbs_procedures.py
+++ cbs_procedures.py
@@ -5,6 +5,30 @@ Created on Tue May 11 11:44:45 2021
 
 @author: ziskin
 """
+from MA_paths import work_david
+
+
+def read_periphery_index(path=work_david):
+    import pandas as pd
+    df = pd.read_excel(
+        path/'peripheri_index_cbs_2015_local_cities.xls', skiprows=8)
+    cols = ['municipal_status', 'city_code', 'NameHe', 'NameEn', 'Pop2015', 'TLV_proximity_value',
+            'TLV_proximity_rank', 'PAI_value', 'PAI_rank', 'P2015_value', 'P2015_rank', 'P2015_cluster', 'P2004_cluster']
+    df.columns = cols
+    df = df.dropna()
+    df['city_code'] = df['city_code'].astype(int)
+    df.set_index('city_code', inplace=True)
+    df_r = pd.read_excel(
+        path/'peripheri_index_cbs_2015_regional_councils.xls', skiprows=6)
+    cols = ['municipal_status', 'RC_nameHe', 'RC_nameEn', 'city_code', 'NameHe', 'NameEn', 'Sub_district', 'Pop2015', 'TLV_distance',
+            'TLV_proximity_value', 'TLV_proximity_rank', 'PAI_value', 'PAI_rank', 'P2015_value', 'P2015_rank', 'P2015_cluster']
+    df_r.columns = cols
+    df_r = df_r.dropna()
+    df_r.set_index('city_code', inplace=True)
+    dff = pd.concat([df, df_r], axis=0)
+    dff.index = dff.index.astype(int)
+    return dff
+
 
 def read_cbs_main_indicies(start_date='1997-01', end_date=None,
                            savepath=None):
@@ -27,6 +51,21 @@ def read_cbs_main_indicies(start_date='1997-01', end_date=None,
         cols = [x for x in item.keys()]
         data.append([x for x in item.values()])
     df = pd.DataFrame(data, columns=cols)
+    df.index = pd.to_datetime(df['date'], format='%Y-%m')
+    dates = pd.to_datetime(df['date'].unique(), format='%Y-%m')
+    dff = pd.DataFrame(index=dates)
+    names = {}
+    for ind_code in df['code'].unique():
+        dff[str(ind_code)] = df[df['code'] == ind_code]['index'].astype(float)
+        dff[str(ind_code)+'_base'] = df[df['code'] == ind_code]['base']
+        names[ind_code] = df[df['code'] == ind_code]['name'].unique().item()
+    dff.index.name = 'time'
+    ds = dff.to_xarray()
+    for da in ds:
+        try:
+            ds[da].attrs['index_name'] = names[da]
+        except KeyError:
+            pass
     # root = ET.XML(r.content)
     # data = []
     # cols = []
@@ -35,4 +74,4 @@ def read_cbs_main_indicies(start_date='1997-01', end_date=None,
     #     cols.append(child.tag)
     # df = pd.DataFrame(data).T  # Write in DF and transpose it
     # df.columns = cols
-    return df
+    return ds
